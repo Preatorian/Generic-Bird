7285d96be3fd8b766c0153f8e4e999d49dc88ae8
Dominik Szczypta <szczyptadominik@gmail.com>
2018-05-28 01:51:14 +0200

    First Commit

---
+++ Generic Bird/Assets/NeuralNet.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using UnityEngine;
+
+namespace Assets
+{
+    public class NeuralNet
+    {
+        public List<float> inputData;
+        float output;
+        public List<List<List<float>>> Layers;
+        System.Random rand;
+        public NeuralNet(List<List<List<float>>> Lists)
+        {
+            Layers = new List<List<List<float>>>(Lists);
+        }
+        public NeuralNet()
+        {
+            rand  = new System.Random();
+            List<float> nn = new List<float>(new float[] { 0, 0, 0, 0, 0, 0, 0, 0, 0, 0 });
+
+            List<List<float>> ii = new List<List<float>>();
+            List<List<List<float>>> xx = new List<List<List<float>>>();
+            
+            for (int i = 0; i < 10; i++)
+            {
+                for (int n = 0; n < 10; n++)
+                {
+                    ii.Add(new List<float>(new float[] 
+                    { rand.Next(-100,100),
+                    rand.Next(-100,100),
+                    rand.Next(-100,100),
+                    rand.Next(-100,100),
+                    rand.Next(-100,100),
+                    rand.Next(-100,100),
+                    rand.Next(-100,100),
+                    rand.Next(-100,100),
+                    rand.Next(-100,100),
+                    rand.Next(-100,100)
+
+                    }));
+                }
+                xx.Add(ii);
+            }
+            Layers = new List<List<List<float>>>(xx);
+        }
+            float activation(float x)
+        {
+            return (float)(1 / (1 + Math.Exp(-x)));
+        }
+        public float getResoult()
+        {
+            int ab = 0;
+            float sum = 0;
+            List<float> LayerLastResoult = new List<float>(inputData);
+            List<float> LayerNewResoult = new List<float>();
+            foreach (List<List<float>> thisLayer in Layers)
+            {
+                LayerNewResoult = new List<float>();
+                foreach (List<float> neuron in thisLayer)
+                {
+                    for (int i = 0; i < neuron.Count; i++)
+                    {
+                        sum += LayerLastResoult[i] * neuron[i];
+                      //  sum += ab/2f*100 * -ab*-10+20;
+                        ab++;
+                    }
+                    LayerNewResoult.Add(activation(sum));
+                    sum = 0;
+
+                }
+                LayerLastResoult = new List<float>(LayerNewResoult);
+
+            }
+            return activation(LayerNewResoult.Sum());
+        }
+    }
+}
---
+++ Generic Bird/Assets/Player.cs
@@ -0,0 +1,67 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+namespace Assets
+{
+    public class Player : MonoBehaviour {
+        private float rayLen = 2.5f;
+        private Vector3 thisPos;
+        private List<Vector2> allDirections;
+        private List<float> Inputs;
+        Rigidbody2D r;
+        Text text;
+        NeuralNet net;
+        void Start() {
+            thisPos = this.transform.position;
+            Inputs = new List<float>(new float[] { 0, 0, 0, 0, 0 });
+            text = GameObject.Find("Text").GetComponent<Text>();
+            r = GetComponent<Rigidbody2D>();
+            allDirections = new List<Vector2>();
+            allDirections.Add(new Vector2(0, 1));
+            allDirections.Add(new Vector2(1, 1));
+            allDirections.Add(new Vector2(1, 0));
+            allDirections.Add(new Vector2(1, -1));
+            allDirections.Add(new Vector2(0, -1));
+
+            net = new NeuralNet();
+        }
+        private void OnDrawGizmos()
+        {
+            Gizmos.DrawRay(transform.position, Vector3.down);
+        }
+
+        void Update()
+        {
+            thisPos = this.transform.position;
+            RaycastHit2D hit;
+            int index = 0;
+            foreach (Vector2 d in allDirections)
+            {
+                hit = Physics2D.Raycast(thisPos, d, rayLen, 1 << 8);
+                if (hit)
+                {
+                    Inputs[index] = hit.distance;
+                }
+                else
+                    Inputs[index] = rayLen;
+                index++;
+            }
+            net.inputData = new List<float>(new float[] { 0,0,0,0,0,0,0,0,0,0});
+            text.text = "";
+            foreach (float val in Inputs)
+            {
+                text.text += val + "\n";
+            }
+            if (Input.GetKeyDown(KeyCode.UpArrow))
+                r.AddForce(new Vector2(0, 300));
+            if (Input.GetKeyDown(KeyCode.LeftArrow))
+                r.AddForce(new Vector2(-100, 0));
+            if (Input.GetKeyDown(KeyCode.RightArrow))
+                r.AddForce(new Vector2(100, 0));
+
+            net.getResoult();
+        }
+    }
+}
